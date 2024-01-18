71beabd986ba490d33dc669fb2d39995412db618
John <johnnkonge2020@gmail.com>
2024-01-18 13:38:29 +0300

    done

--- server/app.py
+++ server/app.py
@@ -70,14 +70,47 @@ class Login(Resource):
                 return user.to_dict(),200
             return {'error':'401,Unauthorized'},401
         
-        
+
             
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id']= None
+        return {},204
+    
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+
+        user = User.query.filter(User.id == session['user_id']).first()
+        return [recipe.to_dict() for recipe in user.recipes], 200
+        
+        
+    def post(self):
+
+            request_json = request.get_json()
+
+            title = request_json['title']
+            instructions = request_json['instructions']
+            minutes_to_complete = request_json['minutes_to_complete']
+
+            try:
+
+                recipe = Recipe(
+                    title=title,
+                    instructions=instructions,
+                    minutes_to_complete=minutes_to_complete,
+                    user_id=session['user_id'],
+                )
+
+                db.session.add(recipe)
+                db.session.commit()
+
+                return recipe.to_dict(), 201
+
+            except IntegrityError:
+
+                return {'error': '422 Unprocessable Entity'}, 422
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
