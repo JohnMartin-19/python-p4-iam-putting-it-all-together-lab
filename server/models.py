931e5a9f236dfd5de8d99e602acec26f80ee428a
John <johnnkonge2020@gmail.com>
2024-01-18 11:06:42 +0300

    updating models

--- server/models.py
+++ server/models.py
@@ -6,9 +6,21 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    serialize_rules = ("-recipes.user",'-_password_hash',)
+
+    id = db.Column(db.Integer(255),primary_key = True)
+    username = db.Column(db.String(64), unique=True, nullable = False)
+    password = db.Column(db.LargeBinary())
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+    id = db.Column(db.Integer)
+    title = db.Column(db.String,nullable = False)
+    user_id = db.Column(db.Integer(64),foreign_key = True)
+    instructions = db.Column(db.String)
+    minutes_to_complete = db.Column(db.Integer >=(50))
+
     
     pass
